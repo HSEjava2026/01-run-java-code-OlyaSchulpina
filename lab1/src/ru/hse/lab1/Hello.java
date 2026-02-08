aba679a11d3326c2c90b1d4bc938f278b93d8bc2
Olya Schulpina <olyaswimmerka16@gmail.com>
2026-02-08 13:26:47 +0500

    Задание 3-4

--- lab1/src/ru/hse/lab1/Hello.java
+++ lab1/src/ru/hse/lab1/Hello.java
@@ -1,5 +1,9 @@
 package ru.hse.lab1;
-
+/**
+ * @author OlyaSchulpina
+ * @version 1.0
+ * @since 2026
+ */
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
--- lab1/src/ru/hse/lab1/Main.java
+++ lab1/src/ru/hse/lab1/Main.java
@@ -1,8 +1,17 @@
 package ru.hse.lab1;
-
+/**
+ * Класс для работы с классом Person
+ * @author OlyaSchulpina
+ * @version 1.0
+ * @since 2026
+ */
 import ru.hse.lab1.second.Person;
 
 public class Main {
+    /**
+     * Главный метод программы
+     * @param args аргументы командной строки
+     */
     public static void main(String[] args) {
         Person person = new Person("Иван", 20);
         System.out.println("Имя: "+ person.name);
